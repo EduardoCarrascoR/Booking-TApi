0588b6a2690f02ffc0f01a9549cf05f465e0d37d
Eduardo Carrasco <blezthor666@gmail.com>
2020-03-11 00:18:45 -0300

    middle-ware
    directory middle-ware and create middle-ware

---
+++ src/middlewares/get-user.middleware.ts
@@ -0,0 +1,31 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { Request, Response } from "express";
+import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+
+
+@Injectable()
+export class GetUserMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: () => void) {
+    const authJwtToken = req.headers.authorization.replace(/^Bearer /, '');
+    if(!authJwtToken) {
+      next();
+      return;
+    }
+
+    try {
+      const user = jwt.verify(authJwtToken, process.env.JWT_SECRET);
+
+      if(user){
+        console.log('Found user details in JWT: ', user);
+        req['user'] = user;
+      }
+    }
+    catch(err) {
+      console.log('Error handling authentication JWT: ', err);
+      
+    }
+
+    next();
+  }
+}
